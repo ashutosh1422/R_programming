661b1b5d89705239303f952feed0f92440ce0c98
ashutosh1422 <73709205+ashutosh1422@users.noreply.github.com>
2021-02-11 16:40:05 +0530

    Add files via upload

---
+++ pactice3.R
@@ -0,0 +1,19 @@
+ x<- matrix(1:9 ,nrow = 3, ncol = 3, byrow  = FALSE)
+x
+n<- matrix(1:6, nrow = 3,ncol = 3, byrow = FALSE)
+n
+n<- matrix(1:6, nrow = 9,ncol = 3, byrow = FALSE)
+n
+
+gender<- factor(c('male','female','female','male'))
+gender
+
+
+cc<- data.frame (
+age<-c(18,14,24)
+sex<- c("F","F","M")
+names<-c("sanyukta","asmita","aditya")
+)
+cc
+result<-data.frame(names$age,names$sex)
+print(result)
